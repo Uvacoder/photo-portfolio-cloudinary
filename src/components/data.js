19f725a539de5d4eb6ec6a39de267f838621e4ba
Uvacoder <uva22940coder@gmail.com>
2022-09-25 22:09:50 -0400

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,23 +1,18 @@
 let mainImages = [
         {
             index: 0,
-            url: "https://res.cloudinary.com/philipscloud/image/upload/v1627228948/Analog/Originals/R1-03298-020A_02_hzlxha.jpg",
-            alt: "Woman watering flowers"
-        },
-        {
-            index: 1,
-            url: "https://res.cloudinary.com/philipscloud/image/upload/v1627220750/Analog/Originals/R1-03298-015A_01_l1ztef.jpg",
-            alt: "Car in gas station"
+            url: "https://res.cloudinary.com/iarenotamnot/image/upload/v1662748960/profilepics/elsa-lookahead.jpg",
+            alt: "Elsa looking ahead"
         },
         {
             index: 2,
-            url: "https://res.cloudinary.com/philipscloud/image/upload/v1627221433/Analog/Originals/R1-03663-0003_02_nj31i7.jpg",
-            alt: "Man in park"
+            url: "https://res.cloudinary.com/iarenotamnot/image/upload/v1662748945/profilepics/elsa-onhorse.png",
+            alt: "Elsa on a horse"
         },
         {
             index: 16,
-            url: "https://res.cloudinary.com/philipscloud/image/upload/v1627918335/Analog/Originals/R1-03928-0026_02_wq7cak.jpg",
-            alt: "Elder couple eating ice cream"
+            url: "https://res.cloudinary.com/iarenotamnot/image/upload/v1662748909/profilepics/elsa-lookback.jpg",
+            alt: "Elsa looking back"
         }
 ];
 
